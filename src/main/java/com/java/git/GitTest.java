86be6e7068e7a2c9074593ec29955c296d1615c7
LHW <1055807337@qq.com>
2022-03-16 15:03:41 +0800

    hot-fix commit2

--- src/main/java/com/java/git/GitTest.java
+++ src/main/java/com/java/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("嗨嗨嗨");
         System.out.println("大队长，是你想陷害我是吧");
         System.out.println("鸡汤来咯");
+        System.out.println("xswl，wc");
         System.out.println("TNND 跟我玩阴的是吧，那...那就来吧");
     }
 }
