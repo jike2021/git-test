13dc324b5514df51be1c09cfa3a2cb1b0c2f38d9
LHW <1055807337@qq.com>
2022-03-16 10:51:26 +0800

    third commit

--- src/main/java/com/java/git/GitTest.java
+++ src/main/java/com/java/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("嗨嗨嗨");
         System.out.println("大队长，是你想陷害我是吧");
+        System.out.println("鸡汤来咯");
     }
 }
