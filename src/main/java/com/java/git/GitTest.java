a008fd7d06f507fcb55455f614dfa2007c761f7b
jike2021 <83819733+jike2021@users.noreply.github.com>
2022-03-17 09:09:38 +0800

    pull commit

--- src/main/java/com/java/git/GitTest.java
+++ src/main/java/com/java/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("嗨嗨嗨");
         System.out.println("大队长，是你想陷害我是吧");
+        System.out.println("我喝我喝，喝汤多是件没事啊");
         System.out.println("鸡汤来咯");
         System.out.println("xswl，wc");
         System.out.println("TNND 跟我玩阴的是吧，那...那就来吧");
@@ -11,5 +12,6 @@ public class GitTest {
         System.out.println("呜哈哈");
         System.out.println("TNND 跟我玩阴的是吧，那...那就来吧");
         System.out.println("嘿休休");
+        System.out.println("啊哈哈，大队长你这人就爱开玩笑");
     }
 }
